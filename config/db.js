5fc075514f9a3f63877fcd7e2f4b0ace1645d508
rasulkhan-RK <rizwandakhan@gmail.com>
2025-06-03 13:02:09 +0530

    update db.js file

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ export default async function connectDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/DeepSeek`)
+      .connect(`${process.env.MONGODB_URI}`)
       .then((mongoose) => mongoose);
   }
   try {
